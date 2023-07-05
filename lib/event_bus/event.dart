7d0d3a651a6976a84a077e3b62b68b36bef40416
Yuri Petrov <48598325+petrovyuri@users.noreply.github.com>
2023-07-05 09:20:24 +0300

    Рефакторинг

--- lib/event_bus/event.dart
+++ lib/event_bus/event.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 /// Родительский класс событий
-class Event<T> extends Equatable {
+abstract class Event<T> extends Equatable {
   const Event(this.data);
 
   final T data;
